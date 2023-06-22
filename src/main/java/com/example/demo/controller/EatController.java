c1d00304e13f18f775ea4fbe80348c4783f57e8a
id <id@AQ-0104>
2023-06-22 16:40:57 +0900

    controller

--- src/main/java/com/example/demo/controller/EatController.java
+++ src/main/java/com/example/demo/controller/EatController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.entity.Eat;
 import com.example.demo.repository.EatRepository;
 
 @Controller
@@ -15,7 +18,10 @@ public class EatController {
 	
 	@GetMapping("/eat")
 	public String index(Model m) {
+	List<Eat> eats = eatRepository.findAll();
 		
+		//Modelクラスに設定
+		m.addAttribute("eats", eats);
 		
 		return "eat" ;
 	}
