409b1c9db1950a8f2cd65515e1a4393405ec681f
id <id@AQ-0104>
2023-06-22 16:00:52 +0900

    sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,51 @@
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('1','No.4','市ヶ谷','洋食','パン','3.72','https://www.tysons.jp/no4/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('2','呑める喫茶店イイジカン','市ヶ谷','洋食','ワインバー','3.57','https://www.instagram.com/iijikan_koujimachi/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('3','グローブ デュ モンド','市ヶ谷','洋食','フレンチ','3.52','https://www.instagram.com/globedumonde/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('4','旬菜ステーキ処 らいむらいと','市ヶ谷','洋食','ハンバーグ','3.61','http://limelight.tokyo/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('5','とんかつ 河むら','市ヶ谷','和食','とんかつ','3.44','https://tonkatsukawamura.owst.jp');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('6','香港華記茶餐廳','市ヶ谷','中華','香港料理','3.34','https://www.facebook.com/wahkeejapan/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('7','タイの食卓 オールドタイランド','市ヶ谷','その他','カレー','3.47','http://www.sscy.co.jp/oldthailand/shopinfo_ichigaya.html');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('8','銀座洋食 三笠會館','池袋','洋食','洋食','3.49','http://www.mikasakaikan.co.jp/restaurant/yoshoku/ginzayoshoku-mikasakaikan/index');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('9','アリヤ 清真美食','池袋','中華','新疆料理','3.43','https://gftv700.gorp.jp/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('10','回し寿司 活 活美登利','池袋','和食','寿司','3.49','https://katumidori.co.jp/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('11','ブラッスリー・ル・リオン','池袋','洋食','フレンチ','3.46','https://lelion.owst.jp/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('12','天下一品','池袋','中華','ラーメン','3.25','http://www.tenkaippin.co.jp/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('13','和食・酒 えん','池袋','和食','居酒屋','3.33','https://en-ikebukuro-parco.com');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('14','ビストロカフェ レディース&ジェントルメン','新宿','洋食','オムライス','3.48','https://www.transit-web.com/content/shops/bistro_cafe_ladiesgentlemen/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('15','Oyster Bar ジャックポット','新宿','洋食','オイスターバー','3.43','https://www.jack-pot.co.jp/brands/oyster_bar_jackpot-shinjuku/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('16','土佐料理 祢保希','新宿','和食','しゃぶしゃぶ','3.47','https://www.katsuo.co.jp/neboke-shinjuku/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('17','添好運','新宿','中華','香港料理','3.49','http://timhowan.jp');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('18','tavern on S <es>','新宿','洋食','アメリカ料理','3.49','http://tavernons.jp/');
+INSERT INTO eat (id,name,area,genre,detai,star,link) VALUES ('19','韓国料理 水刺齋','新宿','その他','韓国料理','3.46','http://suranje.com/');
+
+
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('1','No.4','1','はると','フレンチトーストがふわふわでおいしかったです');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('2','呑める喫茶店イイジカン','2','luck_4413','プリンがとても濃厚で接客がよかった');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('3','グローブ デュ モンド','3','みほこ','ランチセットはデザートプレート付きで超お得。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('4','旬菜ステーキ処 らいむらいと','4','张三','ソースがガーリック風味でおいしかったです。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('5','とんかつ 河むら','5','caesar','市ヶ谷でとんかつといえばここしかない。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('6','香港華記茶餐廳','6','al5.h243o52hq','どんどん味が落ちていく感じがします。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('7','タイの食卓 オールドタイランド','7','弥勒菩薩','オープンテラスの席で解放感が良い。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('8','銀座洋食 三笠會館','8','qwer','最高！その一言に尽きる。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('9','アリヤ 清真美食','4','张三','不像是新疆人开的，像是东北人开的。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('10','回し寿司 活 活美登利','6','al5.h243o52hq','やっぱりチェーん軟化より全然違う！！！');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('11','ブラッスリー・ル・リオン','5','caesar','Too expensive!!!!');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('12','天下一品','1','はると','スープがめちゃくちゃこってりで疲れました。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('13','和食・酒 えん','2','luck_4413','おいしく健康的で心にしみます。。。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('14','ビストロカフェ レディース&ジェントルメン','3','みほこ','卵がふわふわで今まで食べたオムライスの中で一番おいしかったです。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('15','Oyster Bar ジャックポット','4','张三','何度来てもおいしい。カキのピザがおすすめ。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('16','土佐料理 祢保希','5','caesar','クジラのしゃぶしゃぶは初めて。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('17','添好運','6','al5.h243o52hq','値段が高いだけでなく、サービス料も取られました。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('18','tavern on S <es>','7','弥勒菩薩','メインディッシュよりデザートの方が印象的でした。');
+INSERT INTO comments (id, name,bango,namae,comment) VALUES ('19','韓国料理 水刺齋','8','qwer','高島屋にあるのでコスパが悪い。韓国料理なら新大久保の方がいい。');
+
+
+
+INSERT INTO user (bango,namae,password) VALUES ('1','はると','111');
+INSERT INTO user (bango,namae,password) VALUES ('2','luck_4413','222');
+INSERT INTO user (bango,namae,password) VALUES ('3','みほこ','333');
+INSERT INTO user (bango,namae,password) VALUES ('4','张三','444');
+INSERT INTO user (bango,namae,password) VALUES ('5','caesar','555');
+INSERT INTO user (bango,namae,password) VALUES ('6','al5.h243o52hq','666');
+INSERT INTO user (bango,namae,password) VALUES ('7','弥勒菩薩','777');
+INSERT INTO user (bango,namae,password) VALUES ('8','qwer','1234');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,36 @@
+-- テーブル削除
+DROP TABLE IF EXISTS eat;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS user;
+
+
+-- お店のテーブル
+CREATE TABLE eat
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   area TEXT,
+   genre TEXT,
+   detail TEXT,
+   star NUMERIC,
+   link TEXT
+);
+
+
+-- 口コミテーブル
+CREATE TABLE comments
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   comment TEXT
+);
+
+
+-- ユーザーテーブル
+CREATE TABLE user
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  password TEXT
+);
+
