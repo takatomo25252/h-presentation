42eaddd19a0e3f7f6f8dcba5fe5ec35d79866ae5
id <id@AQ-0104>
2023-06-28 10:46:57 +0900

    11

--- src/main/java/com/example/demo/controller/EatController.java
+++ src/main/java/com/example/demo/controller/EatController.java
@@ -32,24 +32,14 @@ public class EatController {
 		
 			if(area==null&&genre==null) {
 				eats=eatRepository.findAll();
-			}else if(area.equals("ikebukuro")&&genre==null) {
-				eats=eatRepository.findByArea(area);
-			}else if(area.equals("ichigaya")&&genre==null) {
-				eats=eatRepository.findByArea(area);
-			}else if(area.equals("shinjuku")&&genre==null) {
-				eats=eatRepository.findByArea(area);
-			}
+			} 
 			
-			if(genre.equals("wa")&&area==null) {
-				eats=eatRepository.findByGenre(genre);
-			}else if(genre.equals("you")&&area==null) {
-				eats=eatRepository.findByGenre(genre);
-			}else if(genre.equals("tyu")&&area==null) {
-				eats=eatRepository.findByGenre(genre);
-			}else if(genre.equals("sonota")&&area==null) {
-				eats=eatRepository.findByGenre(genre);
-			}
-		//Modelクラスに設定
+			else if(area!=null&&genre==null) {
+				eats=eatRepository.findByArea(area);}
+			else if(area==null&&genre!=null) {
+				eats=eatRepository.findByGenre(genre);}
+			
+		
 	
 		m.addAttribute("eats", eats);
 		
